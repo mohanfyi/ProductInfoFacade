31698da80df7761f337106f3cde6deaffa371648
mohanfyi <mohanfyi@users.noreply.github.com>
2015-07-06 18:27:42 +0530

    Update ctl_dashboard.js

--- api/controllers/ctl_dashboard.js
+++ api/controllers/ctl_dashboard.js
@@ -59,7 +59,7 @@ function fnGetProductDetails(req, res) {
     					//product_details.clientinfo = product_detail_client_chunk;
     					logger.info('product_details(JSON)=' + JSON.stringify(product_details));
     					logger.info('product_details=' + product_details);
-    					res.header('Access-Control-Allow-Origin', '*');
+    					res.setHeader('Access-Control-Allow-Origin', '*');
     					res.setHeader('Content-Type', 'application/json');
     					//res.send(JSON.stringify(product_details));
     					res.send(JSON.stringify(product_details));
